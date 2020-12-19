4e6f72d261c7d94dc1ae11ad2751b153550ba887
Stanley <sseeto91@gmail.com>
2020-12-18 22:29:28 -0800

    Initial commit of project files, the scene and some images.

---
+++ Assets/Scripts/Floor.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Floor : MonoBehaviour
+{
+    private BoxCollider2D boxCollider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        boxCollider = GetComponent<BoxCollider2D>();
+        RectTransform rt = GetComponent<RectTransform>();
+        boxCollider.size = new Vector2(rt.rect.width, rt.rect.height);
+    }
+}
